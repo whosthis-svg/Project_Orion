2b7bf4251654ea6e71f0e24a3f5dd5f70434e2bd
whosthis-svg <68013561+whosthis-svg@users.noreply.github.com>
2023-08-09 11:58:22 +1000

    --model ABC

--- DataScience/Models/Action Recognition/Train.py
+++ DataScience/Models/Action Recognition/Train.py
@@ -1,5 +1,131 @@
+import os
+import glob
+from sklearn.preprocessing import MinMaxScaler
+import wandb
+from wandb.keras import WandbMetricsLogger, WandbModelCheckpoint
+from tensorflow.keras.layers import LSTM
+import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models, optimizers
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Activation
-from tensorflow.keras.models import Sequential
+import sklearn
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Masking
+import keras
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+print("Keras version:", keras.__version__)
+print("Tensorflow version:", tf.__version__)
+print("numpy version:", np.__version__)
+print("pandas version:", pd.__version__)
+print("scikit-learn version:", sklearn.__version__)
+
+# if tf.__version__ != '2.10.0':
+#     raise Exception('Incorrect TensorFlow version')
+
+# # Check CUDA version
+# if tf.sysconfig.get_build_info()['cuda_version'] != '11.2':
+#     raise Exception('Incorrect CUDA version')
+
+# # Check cuDNN version
+# if tf.sysconfig.get_build_info()['cudnn_version'] != '8.1':
+#     raise Exception('Incorrect cuDNN version')
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="Project Orion",
+
+    # track hyperparameters and run metadata with wandb.config
+    config={
+        "layer_1": 100,
+        "activation_1": "tanh",
+        "optimizer": "adam",
+        "loss": "sparse_categorical_crossentropy",
+        "metric": "accuracy",
+        "epoch": 30,
+        "batch_size": 64
+    }
+)
+
+class TrainingClass:
+    def __init__(self, data_path=r'E:\Test\A_DeviceMotion_data'):
+        self.data_path = data_path
+        self.model = None
+        self.scaler = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+        print("Num CPUs Available: ", len(tf.config.list_physical_devices('CPU')))
+        print("Num TPUs Available: ", len(tf.config.list_physical_devices('TPU')))
+
+    def load_data(self):
+        sequences = []
+        labels = []
+        max_length = 0
+
+        # Determine max_length without loading all data
+        for folder_path in glob.glob(os.path.join(self.data_path, '*_*')):
+            for subject_file in glob.glob(os.path.join(folder_path, 'sub_*.csv')):
+                with open(subject_file, 'r') as file:
+                    row_count = sum(1 for row in file)-1
+                    max_length = max(max_length, row_count)
+
+        # Loop through all files in the data path and load with padding
+        for folder_path in glob.glob(os.path.join(self.data_path, '*_*')):
+            action = os.path.basename(folder_path).split('_')[0]
+            for subject_file in glob.glob(os.path.join(folder_path, 'sub_*.csv')):
+                df = pd.read_csv(subject_file)
+                df = df.iloc[:, 1:]
+                # Pad the DataFrame with zeros up to max_length rows
+                padded_df = pd.DataFrame(index=range(max_length), columns=df.columns).fillna(0)
+                padded_df.iloc[:len(df)] = df.values
+                sequences.append(padded_df.values)
+                labels.append(action)
+
+        print("Loaded {} sequences".format(len(sequences)))
+        X = np.stack(sequences)
+        y = LabelEncoder().fit_transform(labels)
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.X_train = self.X_train.astype('float')
+        self.X_test = self.X_test.astype('float')
+        print(self.X_train.shape, self.X_train.dtype)
+        print(self.y_train.shape, self.y_train.dtype)
+        print(self.X_test.shape, self.X_test.dtype)
+        print(self.y_test.shape, self.y_test.dtype)
+        
+        
+    def preprocess_data(self):
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.X_train = np.array([self.scaler.fit_transform(x) for x in self.X_train])
+        self.X_test = np.array([self.scaler.transform(x) for x in self.X_test])
+
+    def train_model(self):
+        self.model = Sequential()
+        self.model.add(Masking(mask_value=0., input_shape=(self.X_train.shape[1], self.X_train.shape[2])))
+        self.model.add(LSTM(50, return_sequences=True))  # Return sequences if you want to add more LSTM layers after     
+        self.model.add(LSTM(50, return_sequences=False)) # Last LSTM layer should have return_sequences=False if followed by dense layers
+
+        
+        self.model.add(Dense(6, activation='tanh'))
+
+        self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+        self.model.fit(self.X_train, self.y_train, epochs=30, batch_size=64, validation_data=(self.X_test, self.y_test), callbacks=[early_stopping,WandbMetricsLogger(log_freq=5),WandbModelCheckpoint("models")])
+
+    def save_model(self, file_path='model.h5'):
+        self.model.save(file_path)
+
+TC = TrainingClass()
+TC.load_data()
+TC.preprocess_data()
+TC.train_model()
+TC.save_model()
--- DataScience/Models/Individual Tracking & Monitoring/run.py
+++ DataScience/Models/Individual Tracking & Monitoring/run.py
@@ -8,95 +8,113 @@ import numpy as np
 import utm
 
 
-
-def get_trajectory(user_id, gps_data):
-    # Filter the GPS data for the specific user ID
-    return [point for point in gps_data if point['user_id'] == user_id]
-
-def get_speed(trajectory):
-    speeds = []
-    for i in range(1, len(trajectory)):
-        distance = geodesic(trajectory[i-1]['coordinates'], trajectory[i]['coordinates']).meters
-        time_diff = (trajectory[i]['timestamp'] - trajectory[i-1]['timestamp']).seconds
-        speed = distance / time_diff if time_diff != 0 else 0
-        speeds.append(speed)
-    return speeds
-
-def get_distance(point1, point2):
-    return geodesic(point1['coordinates'], point2['coordinates']).meters
-
-def get_acceleration(speeds):
-    accelerations = []
-    for i in range(1, len(speeds)):
-        time_diff = 1  # Assuming 1-second intervals, you'll need to adapt this
-        acceleration = (speeds[i] - speeds[i-1]) / time_diff
-        accelerations.append(acceleration)
-    return accelerations
-
-def get_stops(trajectory, time_threshold):
-    stops = []
-    for i in range(1, len(trajectory)):
-        if get_distance(trajectory[i-1], trajectory[i]) == 0 and \
-           (trajectory[i]['timestamp'] - trajectory[i-1]['timestamp']).seconds >= time_threshold:
-            stops.append(trajectory[i-1])
-    return stops
-
-def get_mode(speeds, accelerations):
-    avg_speed = np.mean(speeds)
-    avg_acceleration = np.mean(accelerations)
-    # Define logic to determine mode based on speed and acceleration
-    # Example:
-    if avg_speed < 2:
-        return 'walking'
-    elif avg_speed < 15:
-        return 'cycling'
-    else:
-        return 'driving'
+class RealTimeTracking:
+    def get_trajectory(self,user_id, gps_data):
+        # Filter the GPS data for the specific user ID
+        return [point for point in gps_data if point['user_id'] == user_id]
     
-    
-def get_frequent_areas(trajectory, eps, min_samples):
-    coords = [point['coordinates'] for point in trajectory]
-    clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(coords)
-    clusters = {}
-    for i, label in enumerate(clustering.labels_):
-        if label != -1: # Ignore noise
-            if label not in clusters:
-                clusters[label] = []
-            clusters[label].append(trajectory[i])
-    return clusters
+    def get_direction(self, trajectory):
+        directions = []
+        for i in range(1, len(trajectory)):
+            start_point = utm.from_latlon(*trajectory[i-1]['coordinates'])
+            end_point = utm.from_latlon(*trajectory[i]['coordinates'])
+            direction = np.arctan2(end_point[1] - start_point[1], end_point[0] - start_point[0])
+            directions.append(direction)
+        return np.mean(directions)
 
-def get_anomalies(trajectory):
-    coords = [point['coordinates'] for point in trajectory]
-    model = IsolationForest().fit(coords)
-    anomalies = [point for i, point in enumerate(trajectory) if model.predict([coords[i]]) == -1]
-    return anomalies
+    def get_speed(self,trajectory):
+        speeds = []
+        for i in range(1, len(trajectory)):
+            distance = geodesic(trajectory[i-1]['coordinates'], trajectory[i]['coordinates']).meters
+            time_diff = (trajectory[i]['timestamp'] - trajectory[i-1]['timestamp']).seconds
+            speed = distance / time_diff if time_diff != 0 else 0
+            speeds.append(speed)
+        return speeds
 
-def predict_traffic(gps_data):
-    features = []
-    target = []
-    eps=10
-    min_samples=5
-    # Define clusters using DBSCAN or other clustering method
-    clusters = get_frequent_areas(gps_data, eps, min_samples)
+    def get_distance(self,point1, point2):
+        return geodesic(point1['coordinates'], point2['coordinates']).meters
 
-    for cluster in clusters.values():
-        avg_speed = np.mean(get_speed(cluster))
-        avg_direction = np.mean(get_direction(cluster))  # Define this function to calculate direction
-        time = get_time_feature(cluster)  # Define this function to extract relevant time features
+    def get_acceleration(self,speeds):
+        accelerations = []
+        for i in range(1, len(speeds)):
+            time_diff = 1  # Assuming 1-second intervals, you'll need to adapt this
+            acceleration = (speeds[i] - speeds[i-1]) / time_diff
+            accelerations.append(acceleration)
+        return accelerations
 
-        # Combine features
-        features.append([len(cluster), avg_speed, avg_direction, time])
+    def get_stops(self,trajectory, time_threshold):
+        stops = []
+        for i in range(1, len(trajectory)):
+            if self.get_distance(trajectory[i-1], trajectory[i]) == 0 and \
+            (trajectory[i]['timestamp'] - trajectory[i-1]['timestamp']).seconds >= time_threshold:
+                stops.append(trajectory[i-1])
+        return stops
 
-        # Define the target variable, e.g., observed traffic volume for the cluster
-        traffic_volume = get_observed_traffic_volume(cluster)  # Define this function as needed
-        target.append(traffic_volume)
+    def get_mode(self,speeds, accelerations):
+        avg_speed = np.mean(speeds)
+        avg_acceleration = np.mean(accelerations)
+        # Define logic to determine mode based on speed and acceleration
+        # Example:
+        if avg_speed < 2:
+            return 'walking'
+        elif avg_speed < 15:
+            return 'cycling'
+        else:
+            return 'driving'
+        
+        
+    def get_frequent_areas(self,trajectory, eps, min_samples):
+        coords = [point['coordinates'] for point in trajectory]
+        clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(coords)
+        clusters = {}
+        for i, label in enumerate(clustering.labels_):
+            if label != -1: # Ignore noise
+                if label not in clusters:
+                    clusters[label] = []
+                clusters[label].append(trajectory[i])
+        return clusters
 
-    # Create and train the regression model
-    model = LinearRegression()
-    model.fit(features, target)
+    def get_anomalies(self,trajectory):
+        coords = [point['coordinates'] for point in trajectory]
+        model = IsolationForest().fit(coords)
+        anomalies = [point for i, point in enumerate(trajectory) if model.predict([coords[i]]) == -1]
+        return anomalies
 
+    def predict_traffic(self, gps_data):
+        features = []
+        target = []
+        eps = 10
+        min_samples = 5
 
-    return predictions
+        # Define clusters using DBSCAN or other clustering method
+        clusters = self.get_frequent_areas(gps_data, eps, min_samples)
+
+        for cluster in clusters.values():
+            avg_speed = np.mean(self.get_speed(cluster))
+            
+           
+            avg_direction = self.get_direction(cluster)
+            
+            # Extract relevant time features (you might want to define this function based on your requirements)
+            time = self.get_time_feature(cluster)
+            
+            # Combine features
+            features.append([len(cluster), avg_speed, avg_direction, time])
+
+            # Define the target variable, e.g., observed traffic volume for the cluster
+            traffic_volume = self.get_observed_traffic_volume(cluster)  # Define this function as needed
+            target.append(traffic_volume)
+
+        # Create and train the regression model
+        model = LinearRegression()
+        model.fit(features, target)
+
+        # Here you can predict on new data if required
+        # For this example, I'm predicting on the training data itself
+        predictions = model.predict(features)
+
+        return predictions
+
 
 
 
